d31433bb08ae0099910e16f7de676f04de52d79b
효석 이 <lhs19960330@gmail.com>
2025-05-15 15:24:33 +0900

    feat : BinarySearch

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main( string [] args )
+        {
+            int [] arr = { 1, 3, 10, 14, 20, 30 };
+
+            int search = BinartSearch(arr, 20);
+
+            Console.WriteLine(search);
+        }
+
+        static int BinartSearch( int [] arr, int target )
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while ( left <= right )
+            {
+                
+                int mid = left + ( right - left ) / 2;
+                if ( target == arr [mid] )
+                    return mid;
+                if ( target < arr [mid] )
+                {
+                    right = mid - 1;
+                }
+                else if ( target > arr [mid] )
+                {
+                    left = mid + 1;
+                }
+
+            }
+            return -1;
+        }
+    }
+}
